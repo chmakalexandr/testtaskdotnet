acb6a83469087409d36774ad764f5e5004e08c74
alllexxx <freeallex@gmail.com>
2018-09-07 22:56:59 +0300

    Create class Programm with method Main

---
+++ TestTask/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 2)
+                {
+                    string inputFile = args[0];
+                    string outputFile = args[1];
+                               
+                    StreamReader sr = new StreamReader(inputFile);
+
+                    List<Word> selectWords = TextOperations.SelectWords(sr.ReadToEnd());
+
+                    using (StreamWriter sw = File.CreateText(outputFile))
+                    {
+                        sw.WriteLine(TextOperations.PrintGroup(selectWords));
+                    }
+
+                    Console.WriteLine("Операция выполнена успешно. Нажмите любую кнопку ...");
+                }
+                else
+                    {
+                    Console.WriteLine("Параметры заданы не корректно.");
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Нельзя прочесть файл:");
+                Console.WriteLine(e.Message);
+            }
+        }
+   
+    }
+}
