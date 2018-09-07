eede2480639455efdd8368d49dfe36a40dddf0a6
alllexxx <freeallex@gmail.com>
2018-09-07 23:01:32 +0300

    Create class for for comparing entities Word

---
+++ TestTask/DistinctWordComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    class DistinctWordComparer : IEqualityComparer<Word>
+    {
+        public bool Equals(Word w1, Word w2)
+        {
+            return w1.Name == w2.Name && w1.Amount == w2.Amount;
+        }
+
+        public int GetHashCode(Word obj)
+        {
+            return obj.Name.GetHashCode() ^ obj.Amount.GetHashCode();
+        }
+    }
+}
