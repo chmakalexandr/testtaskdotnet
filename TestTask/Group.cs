ded1b6f304bc1c1f7009e6d05483950ad0ef5ecb
alllexxx <freeallex@gmail.com>
2018-09-07 22:59:37 +0300

    Create class for entity Group

---
+++ TestTask/Group.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    class Group
+    {
+        public char Name { get; set; }
+        public List<Word> Words { get; set; }
+
+        public Group(char name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            output = Name + "\r\n";
+            foreach (Word w in Words)
+            {
+                output = output + w.ToString() + "\r\n";
+            }
+
+            return output;
+        }
+    }
+}
