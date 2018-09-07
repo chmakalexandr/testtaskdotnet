69724ffeaa7d11b090f045cc1fea37304625712d
alllexxx <freeallex@gmail.com>
2018-09-07 22:57:40 +0300

    Create class for entity Word

---
+++ TestTask/Word.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    class Word : IComparable<Word>,IEquatable<Word>
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+
+        public int CompareTo(Word word)
+        {
+            if (word == null)
+                return 1;
+            else
+                return this.Name.CompareTo(word.Name);
+        }
+
+        public bool Equals(Word w)
+        {
+            if(w == null || GetType() != w.GetType())
+                return false;
+
+            Word word = (Word)w;
+
+            return (this.Name == word.Name) && (this.Amount == word.Amount);
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Amount;
+        }
+
+    }
+}
